9b743d2ff51032e49ef37881b93ccbf53861d8f2
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-05-25 15:55:36 -0600

    add sample customized html code

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    
+    <h1>Hello React on steriods using the new Next.js World!</h1>
     </div>
   )
 }
